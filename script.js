48de53a670fef4284777b2898a0fda0292a188db
sarahmjk99 <sarah.mjk@gmail.com>
2024-01-09 19:23:00 -0600

    init

---
+++ script.js
@@ -0,0 +1,8 @@
+function animateSkill(element) {
+    element.style.transform = "scale(1.1)";
+    element.style.transition = "transform 0.3s";
+}
+
+function resetSkill(element) {
+    element.style.transform = "scale(1)";
+}
