538727ce818eec7e3edf0b897aa7de459c0d19ae
sarahmjk99 <sarah.mjk@gmail.com>
2024-01-09 19:59:00 -0600

    v3

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function scrollToSection(sectionId) {
-    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
-}
+// Add smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
